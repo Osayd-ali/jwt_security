3b0b9b0923f769f49449b0f9466a681812dbba12
Mir Osayd Ali <mali123@depaul.edu>
2025-10-28 18:14:36 -0500

    Initial project setup

---
+++ src/main/java/edu/ali/jwt_securityFinal/JwtSecurityFinalApplication.java
@@ -0,0 +1,13 @@
+package edu.ali.jwt_securityFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ali/jwt_securityFinal/JwtSecurityFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ali.jwt_securityFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
