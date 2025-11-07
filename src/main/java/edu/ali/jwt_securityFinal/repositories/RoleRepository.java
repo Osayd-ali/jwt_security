e9cc0f0bf7a911bdd7a4397dfce00235052bda8a
Mir Osayd Ali <mali123@depaul.edu>
2025-11-07 16:54:20 -0600

    Repository classes for CRUD operations on Users and Roles

--- src/main/java/edu/ali/jwt_securityFinal/repositories/RoleRepository.java
+++ src/main/java/edu/ali/jwt_securityFinal/repositories/RoleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+// RoleRepository interface for accessing Role entities from the database
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
 }
--- src/main/java/edu/ali/jwt_securityFinal/repositories/UserRepository.java
+++ src/main/java/edu/ali/jwt_securityFinal/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+// UserRepository interface for performing CRUD operations on User entities
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
