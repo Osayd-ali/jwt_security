2a759c46e79cba1d3272952911db0fb5ccfefd59
Mir Osayd Ali <mali123@depaul.edu>
2025-11-07 17:14:00 -0600

    Dto class for returning JWT token, token will be in this format in response

---
+++ src/main/java/edu/ali/jwt_securityFinal/dtos/JwtResponse.java
@@ -0,0 +1,24 @@
+package edu.ali.jwt_securityFinal.dtos;
+
+//Returns JWT token after successful authentication
+// JWT Token response will be in this format
+public class JwtResponse {
+
+    private String token;
+
+    public JwtResponse() {
+    }
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
+
