252f334e147c3de61741b8ccdbc4ca22ee1de339
Mir Osayd Ali <mali123@depaul.edu>
2025-11-07 19:00:00 -0600

    Custom user details class for loading user details from database

---
+++ src/main/java/edu/ali/jwt_securityFinal/services/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package edu.ali.jwt_securityFinal.services;
+
+import edu.ali.jwt_securityFinal.entities.User;
+import edu.ali.jwt_securityFinal.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+// Implementation of CustomUserDetailsService to load user details from the database
+@Service
+public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                        .collect(Collectors.toList())
+        );
+    }
+}
+
